f61bf8d775c58624ae194836722f6ef1a038b9e2
Anvith10 <anvith1919@gmail.com>
2021-09-02 19:28:55 +0530

    console learnt :)

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log('Hey this is my first piece of Js code')
+
+console.log(false,134323,'String',{ name:"Anvith"})
+
+console.warn("Don't press the button!!!!")
+
+console.error("This is an error by me")
