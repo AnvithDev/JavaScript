da1f7f1a0642bd5145270de32ad91f1b8a53bdb6
Anvith10 <anvith1919@gmail.com>
2021-09-10 13:01:05 +0530

    filter array method

--- ES6.js
+++ ES6.js
@@ -22,11 +22,34 @@
 
 // Filter method (Same as map but it return only those elements which match a condition(usually))
 
-let ranks=[1,2,3,4,5,6]
+// let ranks=[1,2,3,4,5,6]
 
-const even =ranks.filter((ele,index)=>{
-         if(ele%2===0)
-             return ele
-     })
+// const even =ranks.filter((ele,index)=>{
+//          if(ele%2===0)
+//              return ele
+//      })
 
-console.log(even)
+// console.log(even)
+
+// -----------------------------------------------------------
+
+// Promises
+
+let concert = false ;
+
+let attendConcert = new Promise(function(resolve,reject){
+    setTimeout(()=> 
+    {
+        if(concert) 
+            resolve("BOB ATTENDED THE CONCERT")
+        else    
+            reject("BOB FAILED TO ATTEND THE CONCERT")
+    }, 2000)
+})
+
+console.log(attendConcert)
+console.log(attendConcert)
+
+attendConcert.then((data)=>console.log(data))
+attendConcert.catch((data)=>console.log(data))
+
