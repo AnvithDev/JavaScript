57cb42b0e2c0c49bfe8e608718c9cc5c852f2eef
Anvith10 <anvith1919@gmail.com>
2021-09-10 12:19:02 +0530

    filter array method

--- ES6.js
+++ ES6.js
@@ -1,18 +1,32 @@
 // Maps (Similar to forEach but it returns the modified array with it unlike forEach)
 
-let ranks=[1,2,3,4]
+// let ranks=[1,2,3,4]
 
-ranks.forEach((element,index) => {let ele=element+2
-    console.log(ele,index)})
+// ranks.forEach((element,index) => {let ele=element+2
+//     console.log(ele,index)})
 
-const modi=ranks.map((element,index)=>{let ele=element+2
-return ele})
+// const modi=ranks.map((element,index)=>{let ele=element+2
+// return ele})
 
-console.log(modi)
+// console.log(modi)
 
-const even =ranks.map((ele,index)=>{
-    if(ele%2===0)
-        return ele
-})
+// const even =ranks.map((ele,index)=>{
+//     if(ele%2===0)
+//         return ele
+// })
+
+// console.log(even)   //Maps work on each element of the array
+                    //In this case in place of 1 and 3 it returns undefined
+
+// -----------------------------------------------------------
+
+// Filter method (Same as map but it return only those elements which match a condition(usually))
+
+let ranks=[1,2,3,4,5,6]
+
+const even =ranks.filter((ele,index)=>{
+         if(ele%2===0)
+             return ele
+     })
 
 console.log(even)
